8376b1e7ead30c1d5b6495eecaf8ebaa1a963edb
Andrew Walker <woxorz@gmail.com>
2014-12-12 18:04:29 -0800

    regression testing on all tests

--- phantomcss-test.js
+++ phantomcss-test.js
@@ -1,16 +1,46 @@
 var phantomcss = require('phantomcss');
 
+var test_files = [
+	'tests/holy-grail-example.html',
+	'tests/horizontal-alignment.html',
+	'tests/marketing-stuff.html',
+	'tests/shifting.html',
+	'tests/text.html',
+	'tests/tile-vs-tiles-justify.html',
+	'tests/vertical-alignment-example.html',
+	'tests/widths.html'
+];
+
+var page_delay = 100; // TODO: Fine tune this number. Can we get away with using 0 instead?
+
+var screen_sizes = [
+	320,
+	768,
+	992,
+	1200,
+	1400
+];
+
 phantomcss.init({
 	screenshotRoot: 'tests/screenshots',
 	failedComparisonsRoot: 'tests/screenshot-failures',
 	libraryRoot: 'node_modules/phantomcss'
 });
 
-casper.start('tests/widths.html');
-casper.viewport(1024,100);
+casper.start();
 
-casper.then(function() {
-	phantomcss.screenshot('html',500,null,'i-dunno');
+screen_sizes.forEach(function(screen_size) {
+	casper.then(function(){
+		casper.viewport(screen_size,100); // TODO: Not sure what to put as the y value here. Does it matter?
+	});
+	test_files.forEach(function(test_file) {
+		casper.thenOpen(test_file);
+		casper.then(function() {
+			var screenshot_name = test_file.replace(/\s|\/|\\/,'-');
+			screenshot_name = screenshot_name+'-'+screen_size;
+			phantomcss.screenshot('html', page_delay, null, screenshot_name);
+		});
+	});
 });
 
 casper.then(function() {
